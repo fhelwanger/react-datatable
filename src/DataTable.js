4086b70655c5f7da3001a1d2e208ed990280a5bf
DiegoPort <diegoportteixeira@gmail.com>
2015-10-14 15:37:20 -0300

    set dinamic data

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Table from './components/Table';
-import SearchBar from './components/SearchBar';
-import Pagination from './components/Pagination';
+import Table from './datatable/Table';
+import SearchBar from './datatable/SearchBar';
+import Pagination from './datatable/Pagination';
 
 export default class DataTable extends React.Component {
   constructor(props) {
@@ -13,7 +13,13 @@ export default class DataTable extends React.Component {
       currentPage: 0,
       pageSize: props.pageSize,
       filteredData: [...props.data]
-    }
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      filteredData: [...nextProps.data]
+    });
   }
 
   onChangeFilter(filter) {
@@ -97,6 +103,7 @@ export default class DataTable extends React.Component {
           onChangePageSize={this.onChangePageSize.bind(this)}
           pageSize={this.state.pageSize} />
         <Table
+          idAttribute={this.props.idAttribute}
           data={this.prepareData()}
           columns={this.props.columns}
           onChangeSort={this.onChangeSort.bind(this)} />
